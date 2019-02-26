6a2905b7d778d827553f673b5adb0a813f8026fe
Sean Pan <xiangpan64@gmail.com>
2019-02-26 10:56:25 -0500

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"log"
 
-	"github.com/xiangpan64/cloud-torrent/server"
-	"github.com/xiangpan64/opts"
+	"github.com/jpillora/cloud-torrent/server"
+	"github.com/jpillora/opts"
 )
 
 var VERSION = "0.0.0-src" //set with ldflags
